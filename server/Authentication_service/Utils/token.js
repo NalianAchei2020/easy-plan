b712cc8caf3b86590cec8a6bbdb1812d92446c4f
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-02-17 06:21:52 +0100

    generates a token

--- server/Authentication_service/Utils/token.js
+++ server/Authentication_service/Utils/token.js
@@ -1,3 +1,9 @@
 import jsonwebtoken from 'jsonwebtoken';
 import { createError } from './error.js';
 import config from '../config.js';
+
+export const generateToken = (user) => {
+  return jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, config.SECRET, {
+    expiresIn: '24h',
+  });
+};
