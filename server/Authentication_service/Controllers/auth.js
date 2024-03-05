a077fd06b517d9de16a75d602977d814d28e9695
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2024-03-05 22:11:39 +0100

    Update auth.js

--- server/Authentication_service/Controllers/auth.js
+++ server/Authentication_service/Controllers/auth.js
@@ -8,5 +8,13 @@ const register = (req, res) => {
     if (existingUser) {
       res.status(409).send({ message: 'User already exist' });
     }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hash,
+      isAdmin: false,
+    });
   } catch (error) {}
 };
