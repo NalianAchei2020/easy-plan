f2141cf4a8d3a2344b9303e937c36b70c02817c4
chiwi2024 <nalianachei2@gmail.com>
2024-07-03 17:20:09 +0100

    Update featureCard.tsx

--- frontend/src/components/featureCard.tsx
+++ frontend/src/components/featureCard.tsx
@@ -6,8 +6,8 @@ interface FeatureCardProps {
 
 const FeatureCard = ({ image, title, description }: FeatureCardProps) => {
   return (
-    <div className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg p-4 mb-4 ">
-      <img src={image} alt={title} className="w-full md:w-1/3 rounded-lg" />
+    <div className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg p-4 mb-16 ">
+      <img src={image} alt={title} className="w-full md:w-1/2 " />
       <div className="p-4">
         <h3 className="text-xl font-bold mb-2">{title}</h3>
         <p className="text-gray-700">{description}</p>
