70c62335bdc8bf8f7991ed2b36979b8578e4fc1c
chiwi2024 <nalianachei2@gmail.com>
2024-07-03 16:07:44 +0100

    Update header.tsx

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <header className="bg-white-50 py-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-2xl font-bold">Logo</div>
+        <div className="text-2xl font-bold">
+          <img src="LOGO 1.png" />
+        </div>
         <nav className="flex items-center space-x-6">
           <Link to="/features" className="text-gray-700">
             Features
