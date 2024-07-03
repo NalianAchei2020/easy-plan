213537ed6bf94d5cb583e5548584695427765b9f
chiwi2024 <nalianachei2@gmail.com>
2024-07-03 15:35:07 +0100

    add links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import Login from './pages/login';
+import Home from './pages/home';
+import Blog from './pages/blog';
+import Helpcenter from './pages/helpcenter';
+import Template from './pages/template';
+import Features from './pages/Features';
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Business Plan</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/templates" element={<Template />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/help-center" element={<Helpcenter />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
