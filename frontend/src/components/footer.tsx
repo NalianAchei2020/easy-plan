ebc3d7e77715983a9f2ffea83c6da3cc549cfc5e
chiwi2024 <nalianachei2@gmail.com>
2024-07-04 17:35:53 +0100

    Update footer.tsx

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,30 +2,48 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <header className="flex flex-row bg-black text-white justify-between">
-      <div className="py-8 px-4 text-[14px]">
-        <span>@ EasyPlan All Rights Reserved</span>
+    <footer className="bg-black text-white py-8 px-4">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between text-[14px]">
+        <div className="mb-4 md:mb-0">
+          <span>@ EasyPlan All Rights Reserved</span>
+        </div>
+        <div>
+          <ul className="flex flex-col md:flex-row gap-4">
+            <li>
+              <Link to="/about" className="text-white hover:text-[#1447FB]">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="text-white hover:text-[#1447FB]">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/Help" className="text-white hover:text-[#1447FB]">
+                Help
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/terms-and-condition"
+                className="text-white hover:text-[#1447FB]"
+              >
+                Terms and Condition
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/privacy-policy"
+                className="text-white hover:text-[#1447FB]"
+              >
+                Privacy Policy
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <ul className="flex flex-row gap-4 py-8 px-4 text-[14px]">
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/Help">Help</Link>
-          </li>
-          <li>
-            <Link to="terms-and-condition">Terms and Condition</Link>
-          </li>
-          <li>
-            <Link to="privacy-policy">Privacy Policy</Link>
-          </li>
-        </ul>
-      </div>
-    </header>
+    </footer>
   );
 };
 
