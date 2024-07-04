6f6ffbf8950020951f2fde6b540252cf59df6a97
chiwi2024 <nalianachei2@gmail.com>
2024-07-04 17:36:03 +0100

    Update templateSECTION.tsx

--- frontend/src/components/templateSECTION.tsx
+++ frontend/src/components/templateSECTION.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const TemplateSECTION = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F5F5F5]">
-      <div className="p-6  text-center">
-        <h2 className="text-2xl font-semibold mb-4">Pre-Made Templates</h2>
-        <p className="text-gray-600 mb-6 mx-16">
+    <div className="flex flex-col items-center px-16 bg-[#F5F5F5]">
+      <div className="p-6">
+        <h2 className="text-2xl font-semibold mb-4 text-center">
+          Pre-Made Templates
+        </h2>
+        <p className="text-gray-600 mb-6 text-center">
           Save time with our ready-to-use templates that cater to diverse
           industries such as finance, technology, and more. Customize them
           instantly to suit your needs and start editing right away.
@@ -60,9 +64,13 @@ const TemplateSECTION = () => {
             className="w-full h-42 object-cover"
           />
         </div>
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">
-          Browse Template
-        </button>
+        <div className="text-center my-8">
+          <Link to="/blog">
+            <button className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-black">
+              Browse Template
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
