cf813f54ba9e9ce278674a1bca4fe0af9b7603a7
chiwi2024 <nalianachei2@gmail.com>
2024-07-04 13:05:17 +0100

    add template section

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,4 +1,5 @@
 import FeaturesSection from '../components/featureSection';
+import TemplateSECTION from '../components/templateSECTION';
 
 const Home = () => {
   return (
@@ -35,6 +36,7 @@ const Home = () => {
         </div>
       </section>
       <FeaturesSection />
+      <TemplateSECTION />
     </>
   );
 };
