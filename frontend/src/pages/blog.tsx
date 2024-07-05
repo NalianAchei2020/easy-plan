ab64c6cf65debd9d1c51eb6f2da4e5469f074821
chiwi2024 <nalianachei2@gmail.com>
2024-07-05 15:22:10 +0100

    Update blog.tsx

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -3,9 +3,9 @@ import BlogSection from '../components/blogSection';
 
 const Blog = () => {
   return (
-    <div className="bg-[#F5F5F5]">
+    <div className="bg-[#F5F5F5] min-h-screen py-12">
       <div className="flex flex-col justify-center items-center mt-8 lg:px-16 sm:px-8">
-        <h1 className="text-[1.5rem]">LEARN FROM OUR BLOG</h1>
+        <h1 className="text-[1.5rem] text-[#073DFA]">LEARN FROM OUR BLOG</h1>
         <BlogSearchSection />
       </div>
       <div>
