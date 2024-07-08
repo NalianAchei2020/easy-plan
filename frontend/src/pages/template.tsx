7972b0fe494a9b9bfccccf86e96658dc63b70d6a
chiwi2024 <nalianachei2@gmail.com>
2024-07-08 14:40:55 +0100

    Update template.tsx

--- frontend/src/pages/template.tsx
+++ frontend/src/pages/template.tsx
@@ -4,7 +4,9 @@ const Template = () => {
   return (
     <div>
       <div>
-        <h1>Choose a template and customize it </h1>
+        <h1 className="text-xl font-[400] mt-10 text-center">
+          Choose a template and customize it{' '}
+        </h1>
       </div>
       <TemplateSECTION />
     </div>
