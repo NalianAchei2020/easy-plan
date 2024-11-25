d26dc15dc41a3030a67b725ddabea268393f1e74
chiwi2024 <nalianachei2@gmail.com>
2024-11-25 07:11:26 +0100

    Update template.js

--- server/templating/controller/template.js
+++ server/templating/controller/template.js
@@ -1,27 +1,27 @@
 import FileUpload from '../modules/template.js';
 
 export const uploadTemplate = async (req, res) => {
-  const { file } = req.body;
-  if (!file) {
+  if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
 
-  // Create a new file upload record
+  const { filename, originalname, mimetype, size } = req.file;
+
   const fileUpload = new FileUpload({
-    filename: file.filename, // Use req.file.filename
-    originalname: file.originalname, // Use req.file.originalname
-    mimetype: file.mimetype, // Use req.file.mimetype
-    size: file.size, // Use req.file.size
-    // userId: req.body.userId, // Uncomment if you need to associate with a user
+    filename,
+    originalname,
+    mimetype,
+    size,
   });
 
   try {
     await fileUpload.save();
     res.status(200).send({
       message: 'Template uploaded successfully!',
-      file: file,
+      file: { filename, originalname, mimetype, size },
     });
   } catch (error) {
+    console.error('Database save error:', error.message);
     res.status(500).send('Error saving file information: ' + error.message);
   }
 };
