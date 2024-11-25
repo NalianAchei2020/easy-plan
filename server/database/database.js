1b103c5cdd02768115fbde609cf94f1a804510b5
chiwi2024 <nalianachei2@gmail.com>
2024-11-25 07:11:21 +0100

    Update database.js

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/easyplan');
+    await mongoose.connect('mongodb://0.0.0.0:27017', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.log('Error connecting to MongoDB', error.message);
