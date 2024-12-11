33904f7790123d74e2d47d07c0fd3b8454efbc06
chiwi2024 <nalianachei2@gmail.com>
2024-12-11 05:17:30 +0100

    add files

--- frontend/src/components/templateSECTION.tsx
+++ frontend/src/components/templateSECTION.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Document, Page, pdfjs } from 'react-pdf';
-
+import PDF from './cv.pdf';
 import { fetchTemplate } from '../redux/templateSlice';
 import { RootState, AppDispatch } from '../redux/store';
 
@@ -32,14 +32,18 @@ const TemplateSECTION = () => {
 
   return (
     <div className="px-6 py-[50px]">
-      <div className="grid grid-cols-5 gap-4 mb-6">
+      <div className="gap-4 mb-6">
         {templates.map((template) => (
-          <div key={template._id} className="w-full h-42">
-            <Document
-              file={template.filename} // Update this path as needed
-              onLoadError={console.error}
-            >
-              <Page pageNumber={1} />
+          <div
+            key={template._id}
+            className="w-25 h-42 p-[50px] bg-gray-500 border-2- border-red"
+          >
+            <Document file={PDF} onLoadError={console.error}>
+              <Page
+                pageNumber={1}
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+              />
             </Document>
           </div>
         ))}
